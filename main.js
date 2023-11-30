bcf4e9e3db82bfea390dfbae52f785a94f5b3930
bismaili-head <74878165+bismaili-head@users.noreply.github.com>
2023-11-30 22:45:03 +0100

    Adds vite+bootstrap boilerplate

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('po bon js');
+import '/src/scss/app.scss';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+	modules: [Navigation, Pagination],
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
