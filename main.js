43b749d6a51085c8f748ef421626699ce320173a
albesademajj <a.demaj@head.wtf>
2023-12-09 20:42:44 +0100

    added Summary and Contact page

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-console.log('po bon js');
 import '/src/scss/app.scss';
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Pagination } from 'swiper';
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper', {
-	modules: [Navigation, Pagination],
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+let swiper = null;
+function initSwiper() {
+	const screenWidth = window.innerWidth;
+	if (screenWidth > 992) {
+		// Initialize Swiper
+		if (!swiper) {
+			swiper = new Swiper('.swiper', {
+				modules: [Pagination],
+				direction: 'vertical',
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+			});
+		}
+	} else {
+		// Destroy Swiper if it's already initialized
+		if (swiper) {
+			swiper.destroy();
+			swiper = null;
+
+			// Manually set styles for the slides and container when Swiper is disabled
+			document.querySelector('.swiper-wrapper').style.transform = 'translate3d(0px, 0px, 0px)';
+			document.querySelectorAll('.swiper-slide').forEach((slide) => {
+				slide.style.width = '';
+				slide.style.height = '';
+			});
+		}
+	}
+}
+// Initial call to set up Swiper based on initial screen width
+initSwiper();
+window.addEventListener('resize', function () {
+	initSwiper();
 });
